02c062d425614063b0f1ca5513d60604663468cd
Katejay <jay.kate20@gmail.com>
2019-11-30 02:52:11 -0800

    File Upload Code

---
+++ Firebase-Storage (File-Upload)/app/src/main/java/com/example/firebasestorage/UploadFile.java
@@ -0,0 +1,24 @@
+package com.example.firebasestorage;
+
+public class UploadFile {
+
+    public String name;
+    public String url;
+
+    public UploadFile() {
+    }
+
+    public UploadFile(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
